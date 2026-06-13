be4d6b801fa9c1bd04bf2d6f2205bf07b69aae72
Elianbarra <El.barram@duocuc.cl>
2026-06-13 11:17:49 -0400

    Change imports in DTO

--- src/main/java/com/hospital/msauth/dto/response/AuthResponseDTO.java
+++ src/main/java/com/hospital/msauth/dto/response/AuthResponseDTO.java
@@ -1,6 +1,10 @@
 package com.hospital.msauth.dto.response;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
